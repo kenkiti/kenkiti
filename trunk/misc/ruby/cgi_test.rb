ba3097d923dbc88e4dc80ffbc42aef794e42fc49
kenkiti <kenkiti@5c13d77d-683a-0410-93b0-0794bd72eb1e>
2008-01-17 16:24:58 +0000

    remove files.

--- trunk/misc/ruby/cgi_test.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-print "Content-type: text/html\n\n"
-
-print "<html><body>Hello World</body></html>"
-
--- trunk/misc/ruby/easy_mecab_test.rb
+++
@@ -1,9 +0,0 @@
-#!/bin/ruby -Ks
-# -*- coding: euc-jp -*-
-require 'easymecab'
-puts open("test.txt").read
-
-m = MeCab.new("-O wakati")
-p m.parse_file("test.txt")
-m = MeCab.new("")
-p m.parse_file("test.txt")
